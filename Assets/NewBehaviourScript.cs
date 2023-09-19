815c3f00b23aee18d120b368163d815d40f6f5ef
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2023-09-19 11:26:35 +0300

    String design

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,17 +5,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerPrefs.
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
 
 [Serializable]
