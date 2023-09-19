ca1a8b63c26d4b12f2ca80bfc8e7ee782ac544c5
Pavel Bu <73419843+puchik006@users.noreply.github.com>
2023-09-19 15:37:55 +0300

    Save and Load

---
+++ Assets/ButtonExtensions.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Events;
+using UnityEngine.UI;
+
+public static class ButtonExtensions
+{
+    public static void Add(this Button button, UnityAction action) => button.onClick.AddListener(action);
+    public static void RemoveAllListeners(this Button button) => button.onClick.RemoveAllListeners();
+}
---
+++ Assets/TableStringHandler.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TableStringHandler : MonoBehaviour
+{
+    private int _id = 0;
+    [SerializeField] private TMP_InputField _name;
+    [SerializeField] private TMP_InputField _type;
+    [SerializeField] private TMP_InputField _speaker;
+    [SerializeField] private TMP_InputField _place;
+    [SerializeField] private TMP_InputField _description;
+    [SerializeField] private TMP_InputField _start;
+    [SerializeField] private TMP_InputField _end;
+    [SerializeField] private TMP_Dropdown _day;
+    [SerializeField] private TMP_Dropdown _month;
+    [SerializeField] private Button _save;
+    [SerializeField] private Button _add;
+    [SerializeField] private Button _delete;
+
+    private void Awake()
+    {
+        FindLastId();
+
+        _save.Add(() => Save());
+    }
+
+    private void FindLastId()
+    {
+        int lastId = 0;
+        while (PlayerPrefs.HasKey("TableData_" + _id.ToString()));
+        {
+            lastId++;
+        }
+        _id = lastId;
+
+        Debug.Log(_id);
+    }
+
+    private void Save()
+    {
+        TableData tableData = new TableData
+        {
+            Name = _name.text,
+            Type = _type.text,
+            Speaker = _speaker.text,
+            Place = _place.text,
+            Description = _description.text,
+            TimeStart = _start.text,
+            TimeEnd = _end.text,
+            Day = _day.value,     
+            Month = _month.value
+        };
+
+        string jsonData = JsonUtility.ToJson(tableData);
+
+        PlayerPrefs.SetString("TableData_" + _id.ToString(), jsonData);
+
+        Debug.Log(jsonData);
+    }
+
+    private void Load()
+    {
+        string jsonData = PlayerPrefs.GetString("TableData_" + _id);
+
+        if (!string.IsNullOrEmpty(jsonData))
+        {
+            TableData tableData = JsonUtility.FromJson<TableData>(jsonData);
+
+            _name.text = tableData.Name;
+            _type.text = tableData.Type;
+            _speaker.text = tableData.Speaker;
+            _place.text = tableData.Place;
+            _description.text = tableData.Description;
+            _start.text = tableData.TimeStart;
+            _end.text = tableData.TimeEnd;
+            _day.value = tableData.Day;
+            _month.value = tableData.Month;
+        }
+        else
+        {
+            Debug.LogWarning("No data found for _id: " + _id);
+        }
+    }
+}
+
+[Serializable]
+public class TableData
+{
+    public string Name;
+    public string Type;
+    public string Speaker;
+    public string Place;
+    public string Description;
+    public string TimeStart;
+    public string TimeEnd;
+    public int Day;
+    public int Month;
+}
+
+
+public class TableDataManager: MonoBehaviour
+{
+    public List<TableData> data;
+}
