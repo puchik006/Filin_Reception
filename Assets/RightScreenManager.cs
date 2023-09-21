d79250eb5b69180b06b7f1f538e723cab16fe739
Pavel Bu <puchik006@mail.ru>
2023-09-21 19:11:58 +0300

    RightScreenManagerContinue

---
+++ Assets/RightScreenManager.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RightScreenManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _todayDate;
+    [SerializeField] private GameObject _firstString;
+    [SerializeField] private GameObject _secondString;
+    [SerializeField] private GameObject _thirdString;
+    private List<int> _todaySchedule = new();
+    private int currentPage = 0;
+
+    private void Awake()
+    {
+        ShowTodayDate();
+        FillTodaySchedule();
+        StartCoroutine(ShowTodaySchedule());
+    }
+
+    private void ShowTodayDate()
+    {
+        DateTime currentDate = DateTime.Now;
+        string russianDate = currentDate.ToString("dd MMMM", new System.Globalization.CultureInfo("ru-RU"));
+        _todayDate.text = russianDate;
+    }
+
+    private void FillTodaySchedule()
+    {
+        DateTime currentTime = DateTime.Now;
+
+        int id = 0;
+
+        while (PlayerPrefs.HasKey("TableData_" + id.ToString()))
+        {
+            string json = PlayerPrefs.GetString("TableData_" + id.ToString());
+
+            TableData tableData = JsonUtility.FromJson<TableData>(json);
+
+            if (tableData.Day == currentTime.Day && tableData.Month == currentTime.Month)
+            {
+                _todaySchedule.Add(id);
+            }
+
+            id++;
+        }
+    }
+
+    private IEnumerator ShowTodaySchedule()
+    {
+        Debug.Log(_todaySchedule.Count);
+
+        while (true)
+        {
+            int pageQty = _todaySchedule.Count % 3 == 0 ? _todaySchedule.Count / 3 : (_todaySchedule.Count / 3) + 1;
+
+            yield return new WaitForSeconds(3);
+            currentPage++;
+            if (currentPage >= pageQty) currentPage = 0;
+
+            if (currentPage * 3 < _todaySchedule.Count)
+            {
+                _firstString.GetComponent<ScheduleStringHandler>().Load(_todaySchedule[0 + currentPage]);
+            }
+            else
+            {
+                _firstString.SetActive(false);
+            }
+
+            if (currentPage * 3 + 1 < _todaySchedule.Count)
+            {
+                _secondString.GetComponent<ScheduleStringHandler>().Load(_todaySchedule[1 + currentPage]);
+                _secondString.SetActive(true);
+            }
+            else
+            {
+                _secondString.SetActive(false);
+            }
+
+            if (currentPage * 3 + 2 < _todaySchedule.Count)
+            {
+                _thirdString.GetComponent<ScheduleStringHandler>().Load(_todaySchedule[2 + currentPage]);
+                _thirdString.SetActive(true);
+            }
+            else
+            {
+                _thirdString.SetActive(false);
+            }
+        }
+    }
+
+}
---
+++ Assets/ScheduleStringHandler.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScheduleStringHandler: MonoBehaviour
+{
+    [SerializeField] private TMP_Text _start;
+    [SerializeField] private TMP_Text _finish;
+    [SerializeField] private TMP_Text _speaker;
+    [SerializeField] private TMP_Text _hall;
+    [SerializeField] private TMP_Text _type;
+    [SerializeField] private TMP_Text _name;
+
+    public void Load(int id)
+    {
+        string jsonData = PlayerPrefs.GetString("TableData_" + id.ToString());
+
+        if (!string.IsNullOrEmpty(jsonData))
+        {
+            TableData tableData = JsonUtility.FromJson<TableData>(jsonData);
+
+            _name.text = tableData.Name;
+            _type.text = tableData.Type;
+            _speaker.text = tableData.Speaker;
+            _hall.text = tableData.Place;
+            _start.text = tableData.TimeStart;
+            _finish.text = tableData.TimeEnd;
+        }
+        else
+        {
+            Debug.LogWarning("No data found for _id: " + id);
+        }
+    }
+}
