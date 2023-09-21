211c6311227a46c3064a111889605aa34becf515
Pavel Bu <puchik006@mail.ru>
2023-09-21 15:08:28 +0300

    Left screen begin

---
+++ Assets/LeftScreenManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class LeftScreenManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+    private int currentPage = 1;
+
+    private void Start()
+    {
+        _text.pageToDisplay = currentPage;
+        StartCoroutine(AutoChangePage());
+    }
+
+    private IEnumerator AutoChangePage()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            currentPage++;
+            if (currentPage > _text.textInfo.pageCount) currentPage = 1;
+            _text.pageToDisplay = currentPage;
+        }
+    }
+}
