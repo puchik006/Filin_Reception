0a5023f359796e8492b78457f41ea54a5931bb04
Pavel Bu <puchik006@mail.ru>
2023-12-07 03:48:53 +0200

    Current time

--- Assets/Scripts/LeftScreenManager.cs
+++ Assets/Scripts/LeftScreenManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -32,10 +33,75 @@ public class LeftScreenManager : MonoBehaviour
         SaveAll.SaveAllData += SaveAndUpdate;
     }
 
+    private void Start()
+    {
+        // Start the coroutine to check conditions every 10 seconds
+        StartCoroutine(CheckConditionsPeriodically());
+    }
+
+    private IEnumerator CheckConditionsPeriodically()
+    {
+        while (true)
+        {
+            CheckConditions();
+            yield return new WaitForSeconds(10f); // Adjust the interval as needed
+        }
+    }
+
+    private void CheckConditions()
+    {
+        // Assuming you have some IDs to iterate over
+        for (int id = 0; id <= 5; id++)
+        {
+            string key = "TableData_" + id;
+
+            if (PlayerPrefs.HasKey(key))
+            {
+                // Deserialize the stored data for the current ID
+                string serializedData = PlayerPrefs.GetString(key);
+                // Assuming you have a class like TableData with properties like TimeStart, TimeEnd, and Name
+                TableData tableData = JsonUtility.FromJson<TableData>(serializedData);
+
+                // Check if the current time is between TimeStart and TimeEnd
+                DateTime currentTime = DateTime.Now;
+                string timeStart = tableData.TimeStart;
+                string timeEnd = tableData.TimeEnd;
+
+                if (!string.IsNullOrEmpty(timeStart) && !string.IsNullOrEmpty(timeEnd) &&
+                    IsValidTimeFormat(timeStart) && IsValidTimeFormat(timeEnd) &&
+                    IsCurrentTimeInRange(currentTime, timeStart, timeEnd) &&
+                    !string.IsNullOrEmpty(tableData.Name))
+                {
+                    // Set _rightNowName to the Name from the stored data
+                    _rightNowName.text = tableData.Name;
+                    // If you want to break out of the loop after finding the first match, you can use 'break;'
+                    break;
+                }
+            }
+        }
+    }
+
+    // Helper method to check if a time string is in the "HH:mm" format
+    private bool IsValidTimeFormat(string timeString)
+    {
+        DateTime dummyResult;
+        return DateTime.TryParseExact(timeString, "HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out dummyResult);
+    }
+
+    // Helper method to check if the current time is between TimeStart and TimeEnd
+    private bool IsCurrentTimeInRange(DateTime currentTime, string timeStart, string timeEnd)
+    {
+        TimeSpan startTime = TimeSpan.Parse(timeStart);
+        TimeSpan endTime = TimeSpan.Parse(timeEnd);
+        TimeSpan currentTimeOfDay = currentTime.TimeOfDay;
+
+        return currentTimeOfDay >= startTime && currentTimeOfDay <= endTime;
+    }
+
     private void SaveAndUpdate()
     {
         ShowTodayDate();
-        _rightNowName.text = _rightNowNameInput.text;
+        //_rightNowName.text = _rightNowNameInput.text;
         _rightNowText.text = _rightNowTextInput.text;
 
         PlayerPrefs.SetString(_keyName, _rightNowNameInput.text);
@@ -46,7 +112,7 @@ public class LeftScreenManager : MonoBehaviour
     {
         if (PlayerPrefs.HasKey(_keyName) && PlayerPrefs.HasKey(_keyText))
         {
-            _rightNowName.text = PlayerPrefs.GetString(_keyName);
+            //_rightNowName.text = PlayerPrefs.GetString(_keyName);
             _rightNowText.text = PlayerPrefs.GetString(_keyText);
 
             _rightNowNameInput.text = PlayerPrefs.GetString(_keyName);
